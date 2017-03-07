c9e9ce1be7f3ed1372af68a9e62906717727ab88
holinder4s <holinder4s@gmail.com>
2017-03-07 09:41:14 +0900

    First test commit - wjdebug

---
+++ sosisul_test0/src/sosisul_test0/test_main.java
@@ -0,0 +1,10 @@
+package sosisul_test0;
+
+public class test_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World ~@@");
+		System.out.println("java eclipse & github test~!!!!");
+	}
+}
